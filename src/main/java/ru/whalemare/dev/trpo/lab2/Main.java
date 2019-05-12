650cd15040b63cbacb231529662955da830fa2a1
whalemare <nanostet@gmail.com>
2019-05-12 12:34:13 +0700

    Add lost files

---
+++ src/main/java/ru/whalemare/dev/trpo/lab2/Main.java
@@ -0,0 +1,30 @@
+package ru.whalemare.dev.trpo.lab2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Anton Vlasov - whalemare
+ * @since 2019
+ */
+public class Main extends Application {
+
+    public Main() {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/scene.fxml"));
+        primaryStage.setTitle("LinkedDequeView");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+}
