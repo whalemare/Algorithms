69188c744c2fbf7e035cedde1c18b3b179fc23e9
whalemare <nanostet@gmail.com>
2019-05-12 12:47:59 +0700

    Add scala test working

---
+++ src/main/scala/ru/whalemare/dev/trpo/lab3/LinkedDequeScala.scala
@@ -0,0 +1,170 @@
+package ru.whalemare.dev.trpo.lab3
+
+import java.util.NoSuchElementException
+
+import com.sun.istack.internal.Nullable
+
+class LinkedDequeScala[Item] extends Iterable[Item] {
+
+  case class Node[T](item: T, @Nullable var next: Node[T])
+
+  var mSize: Int = 0
+
+  @Nullable
+  private var mHead: Node[Item] = _
+
+  @Nullable
+  private var mLast: Node[Item] = _
+
+  override def isEmpty: Boolean = mHead == null
+
+  def peekHead: Item = {
+    if (mHead == null) throw new NoSuchElementException("Queue underflow")
+    mHead.item
+  }
+
+  def peekLast: Item = {
+    if (mLast == null) throw new NoSuchElementException("Queue underflow")
+    mLast.item
+  }
+
+  def addHead(item: Item): Unit = {
+    val prevHead = mHead
+    val newNode = new Node[Item](item, prevHead)
+    mHead = newNode
+    if (prevHead == null) mLast = newNode
+    mSize += 1
+  }
+
+  def addLast(item: Item): Unit = {
+    val l = mLast
+    val newNode = Node(item, null)
+    mLast = newNode
+    if (l == null) mHead = newNode
+    else l.next = newNode
+    mSize += 1
+  }
+
+  def removeHead: Item = {
+    if (isEmpty) throw new NoSuchElementException("Queue is empty")
+    val item = mHead.item
+    mHead = mHead.next
+    mSize -= 1
+    if (isEmpty) mLast = null
+    item
+  }
+
+  def removeLast: Item = {
+    if (mLast == null) throw new NoSuchElementException("Queue is empty")
+    val element = mLast.item
+    if (mHead eq mLast) {
+      mHead = null
+      mLast = null
+    }
+    else {
+      var prevToLast = mHead
+      while ( {
+        prevToLast.next ne mLast
+      }) prevToLast = prevToLast.next
+      mLast = prevToLast
+      mLast.next = null
+    }
+    mSize -= 1
+    element
+  }
+
+  def indexOf(item: Item): Int = {
+    var index = 0
+    if (item == null) {
+      return mSize
+    } else {
+      var x = mHead
+      while (x != null) {
+        if (item == x.item) return index
+        index += 1
+
+        x = x.next
+      }
+    }
+    -1
+  }
+
+  override def toString: String = {
+    val s = new StringBuilder
+    var skipped = false
+    for (item <- this) {
+      if (skipped) s + "->" + item
+      else s + item.toString
+      skipped = true
+    }
+    s.toString
+  }
+
+  private def sortedMerge(a: Node[Item], b: Node[Item], comparator: (Item, Item) => Int): Node[Item] = {
+    var result: Node[Item] = null
+    if (a == null) return b
+    if (b == null) return a
+    if (comparator(a.item, b.item) <= 0) {
+      result = a
+      result.next = sortedMerge(a.next, b, comparator)
+    }
+    else {
+      result = b
+      result.next = sortedMerge(a, b.next, comparator)
+    }
+    result
+  }
+
+  def sort(comparator: (Item, Item) => Int): Unit = {
+    mergeSort(mHead, comparator)
+  }
+
+  private def mergeSort(h: Node[Item], comparator: (Item, Item) => Int): Node[Item] = { // Base case : if head is null
+    if (h == null || h.next == null) return h
+    // get the middle of the list
+    val middle = getMiddle(h)
+    val nextofmiddle = middle.next
+    // set the next of middle node to null
+    middle.next = null
+    // Apply mergeSort on left list
+    val left = mergeSort(h, comparator)
+    // Apply mergeSort on right list
+    val right = mergeSort(nextofmiddle, comparator)
+    // Merge the left and right lists
+    val sortedlist = sortedMerge(left, right, comparator)
+    sortedlist
+  }
+
+  // Utility function to get the middle of the linked list
+  private def getMiddle(h: Node[Item]): Node[Item] = { // Base case
+    if (h == null) return h
+    var fastptr = h.next
+    var slowptr = h
+    // Move fastptr by two and slow ptr by one
+    // Finally slowptr will point to middle node
+    while ( {
+      fastptr != null
+    }) {
+      fastptr = fastptr.next
+      if (fastptr != null) {
+        slowptr = slowptr.next
+        fastptr = fastptr.next
+      }
+    }
+    slowptr
+  }
+
+  override def iterator = new ListIterator(mHead)
+
+  class ListIterator(var current: Node[Item]) extends Iterator[Item] {
+    override def hasNext: Boolean = current != null
+
+    override def next: Item = {
+      if (!hasNext) throw new NoSuchElementException
+      val item = current.item
+      current = current.next
+      item
+    }
+  }
+
+}
